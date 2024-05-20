b459435f5e4e582522b9c650eeee8fced3fb9000
rjcalifornia <rodolfohernan20@gmail.com>
2024-05-19 21:37:50 -0600

    Changes to be committed: 	modified:   lib/ui/screens/login.dart 	modified:   linux/flutter/generated_plugin_registrant.cc 	modified:   linux/flutter/generated_plugins.cmake 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml 	deleted:    test/widget_test.dart 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,21 +11,36 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-        double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: Color(0xff314BCE),
       body: GestureDetector(
-
         onTap: () {
           FocusScope.of(context).requestFocus(FocusNode());
         },
-
         child: SingleChildScrollView(
-          child: Column(children: [
-
-            SizedBox(height: 60,)
-          ],),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Pinput(
+                        length: 6,
+                        showCursor: true,
+                        onCompleted: (pin) => print(pin),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
