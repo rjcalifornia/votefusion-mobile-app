074f757f31a90a019436c29ea928c270c570b812
rjcalifornia <rodolfohernan20@gmail.com>
2024-05-19 22:01:47 -0600

    Changes to be committed: 	modified:   lib/ui/screens/login.dart

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -25,20 +25,77 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 60,
               ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Pinput(
-                        length: 6,
-                        showCursor: true,
-                        onCompleted: (pin) => print(pin),
-                      )
-                    ],
-                  ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 14.0, horizontal: 14.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height / 2,
+                      width: MediaQuery.of(context).size.width / 1,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 14.0, horizontal: 14.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Código de Autenticación",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 31,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Pinput(
+                            length: 4,
+                            showCursor: true,
+                            defaultPinTheme: PinTheme(
+                              width: 60,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  color: Colors.grey.shade100,
+                                ),
+                              ),
+                              textStyle: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            onCompleted: (pin) => print(pin),
+                          ),
+                          const SizedBox(
+                            height: 42,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 12),
+                            ),
+                            onPressed: () {},
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Continuar",
+                                    style: TextStyle(
+                                        fontSize: 22, color: Colors.black)),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-              )
+              ),
             ],
           ),
         ),
